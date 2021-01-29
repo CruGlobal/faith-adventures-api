f54edb77f58b9966ec5629d99b11815d87fd3c3d
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-01-29 13:15:35 +1300

    fix rubocop

--- db/migrate/20210128215938_add_after_response_message_to_adventure_steps.rb
+++ db/migrate/20210128215938_add_after_response_message_to_adventure_steps.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddAfterResponseMessageToAdventureSteps < ActiveRecord::Migration[6.0]
   def change
     add_column :adventure_steps, :after_response_message, :string
