a0de08f9787aa8a4813be5f58e131c6f5cd13d6c
Tataihono Nikora <tataihono.nikora@gmail.com>
2021-03-08 13:37:08 +1300

    simplify firebase id token definition

--- config/initializers/firebase_id_token.rb
+++ config/initializers/firebase_id_token.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 FirebaseIdToken.configure do |config|
-  config.project_ids = [ENV.fetch('FIREBASE_PROJECT_ID')]
+  config.project_ids =
+    if ENV['FIREBASE_PROJECT_ID']
+      [ENV['FIREBASE_PROJECT_ID']]
+    else
+      %w[
+        voke-6ee71
+        faith-adventures
+      ]
+    end
 end
